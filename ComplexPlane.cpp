1c4fa40afd205f0c7af625615583d821460fd7fc
avonhat101 <51030498+avonhat101@users.noreply.github.com>
2024-11-27 11:25:48 -0800

    Update ComplexPlane.cpp

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -145,9 +145,9 @@ void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
 	}
 	else if (count >= 0)
 	{
-		r = 85;
-		g = 182;
-		b = 204;
+		r = 240;
+		g = 20;
+		b = 20;
 	}
 }
 Vector2f ComplexPlane::mapPixelToCoords(Vector2i mousePixel)
