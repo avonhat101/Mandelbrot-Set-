d71e57113a0e3a8301d27c2df4c91774cd5bde3f
avonhat101 <51030498+avonhat101@users.noreply.github.com>
2024-11-26 21:27:59 -0800

    Update ComplexPlane.cpp

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -5,8 +5,10 @@ ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
 	m_pixel_size.x = pixelWidth;
 	m_pixel_size.y = pixelHeight;
 	m_aspectRatio = (float)pixelHeight / (float)pixelWidth;
-	m_plane_center{ 0,0 };
-	m_plane_size{ BASE_WIDTH, BASE_HEIGHT * m_aspectRatio };
+	m_plane_center.x = 0;
+	m_plane_center.y = 0;
+	m_plane_size.x = BASE_WIDTH;
+	m_plane_size.y = BASE_HEIGHT * m_aspectRatio;
 	m_zoomCount = 0;
 	m_state = State::CALCULATING;
 	m_vArray.setPrimitiveType(Points);
@@ -21,7 +23,8 @@ void ComplexPlane::zoomIn()
 	m_zoomCount++;
 	float x = BASE_WIDTH * (pow(BASE_ZOOM, m_zoomCount));
 	float y = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_zoomCount));
-	m_plane_size{ x,y };
+	m_plane_size.x = x;
+	m_plane_size.y = y;
 	m_state = State::CALCULATING;
 }
 void ComplexPlane::zoomOut()
