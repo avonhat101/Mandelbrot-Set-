4232d84863196974c3de5043d5cf432d1be3ba0d
Sploucht <remycwright@gmail.com>
2024-11-27 10:21:58 -0800

    Update ComplexPlane.cpp
    Implement reset function to have default constructor values

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -14,6 +14,20 @@ ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
 	m_vArray.setPrimitiveType(Points);
 	m_vArray.resize(pixelWidth * pixelHeight);
 }
+void ComplexPlane::reset()
+{
+	m_pixel_size.x = pixelWidth;
+	m_pixel_size.y = pixelHeight;
+	m_aspectRatio = (float)pixelHeight / (float)pixelWidth;
+	m_plane_center.x = 0;
+	m_plane_center.y = 0;
+	m_plane_size.x = BASE_WIDTH;
+	m_plane_size.y = BASE_HEIGHT * m_aspectRatio;
+	m_zoomCount = 0;
+	m_state = State::CALCULATING;
+	m_vArray.setPrimitiveType(Points);
+	m_vArray.resize(pixelWidth * pixelHeight);
+}
 void ComplexPlane::draw(RenderTarget& target, RenderStates states) const
 {
 	target.draw(m_vArray);
